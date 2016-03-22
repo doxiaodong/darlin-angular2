182225d459c8ab92e8ff69241a4c591897475fe3
duxiaodong <duxiaodong@darlin.me>
2016-03-22 22:29:42 +0800

    add navbar style

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
-import {Component} from 'angular2/core';
+import {Component, ViewEncapsulation} from 'angular2/core';
 import {Title} from 'angular2/platform/browser';
 
 import {NavbarComponent} from './navbar/navbar.component';
 
 @Component({
+  encapsulation: ViewEncapsulation.None,
   selector: 'root-app',
   template: require('./app.template.html'),
+  styles: [
+    require('./base/styles/global.less')
+  ],
   providers: [Title],
   directives: [NavbarComponent]
 })
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,15 +1,29 @@
 import {Component} from 'angular2/core';
 
+import {User} from 'app/user/user.interface'
+import {USER} from 'app/user/user.mock'
+
 @Component({
   selector: '[navbar]',
   template: require('./navbar.template.html'),
   styles: [
-    require('./navbar.css'),
     require('./navbar.less')
   ]
 })
 export class NavbarComponent {
-  public index = 0;
+  public index: number = 0;
+  public user: User;
+  public isSignin: boolean = true;
+
+  signIn() {
+    console.log('singin');
+  }
+
+  signOut() {
+    console.log('signout');
+  }
+
   constructor() {
+    this.user = USER;
   }
 }
