7be9b348d027044e6474a85b211004093be3d275
duxiaodong <duxiaodong@darlin.me>
2016-04-18 19:42:13 +0800

    use es6

--- config/webpack-plugin/image-minify.js
+++ config/webpack-plugin/image-minify.js
@@ -1,24 +1,25 @@
+"use strict"; // es6 need strict
+
 var path = require('path');
 var Imagemin = require('imagemin');
 
-function ImageMinifyPlugin(options) {
-  this.src = options.src;
-  this.dest = options.dest;
+class ImageMinifyPlugin {
+
+  apply(compiler) {
+    new Imagemin()
+    .src(path.join(this.src, '**/*.{gif,jpg,png,svg}'))
+    .dest(this.dest)
+    .use(Imagemin.jpegtran({progressive: true}))
+    .run((err, files) => {
+        console.log(' image minify');
+    });
+  }
+
+  constructor(options) {
+    this.src = options.src;
+    this.dest = options.dest;
+  }
 
 }
-
-ImageMinifyPlugin.prototype.apply = function(compiler) {
-
-  var _context = compiler.options.context;
-  var _path = path.resolve(_context, this.src);
-  new Imagemin()
-  .src(path.join(this.src, '**/*.{gif,jpg,png,svg}'))
-  .dest(this.dest)
-  .use(Imagemin.jpegtran({progressive: true}))
-  .run((err, files) => {
-      console.log(' image minify');
-  });
-
-};
 
 module.exports = ImageMinifyPlugin;
