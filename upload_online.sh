9c5330aa960dc0257d55b48f772d4e5bf584f5e4
duxiaodong <duxiaodong@darlin.me>
2016-08-30 15:42:27 +0800

    add Makefile

--- upload_online.sh
+++
@@ -1,5 +0,0 @@
-tar czf darlin_me.tar.gz dist
-scp darlin_me.tar.gz root@darlin.me:/var/www/darlin-angular2
-rm darlin_me.tar.gz
-
-ssh root@darlin.me 'cd /var/www/darlin-angular2;rm -rf dist;tar xvzf darlin_me.tar.gz'
