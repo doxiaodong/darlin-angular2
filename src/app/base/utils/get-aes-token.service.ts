fead1db27a73cd70cecacdb5a191f6253b05c916
darlin <duxiaodong@darlin.local>
2016-12-30 19:17:50 +0800

    reset crypto-js

--- src/app/base/utils/get-aes-token.service.ts
+++ src/app/base/utils/get-aes-token.service.ts
@@ -1,7 +1,5 @@
 import { getCookie } from './get-cookie.service'
-// const md5 = require('crypto-js/md5')
-import * as cryptoJS from 'crypto-js'
-const md5 = cryptoJS.MD5
+const md5 = require('crypto-js/md5')
 
 export function getAESToken() {
   return md5(getCookie('csrftoken')).toString()
