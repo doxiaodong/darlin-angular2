26823d0bfdbbeb9aab95f72ee2a49abdf9079cb8
darlin <duxiaodong@darlin.me>
2017-01-05 17:50:27 +0800

    update manifest

--- config/head-config.common.js
+++ config/head-config.common.js
@@ -1,18 +1,18 @@
 module.exports = {
   link: [
     /** <link> tags for 'apple-touch-icon' (AKA Web Clips). **/
-    { rel: 'apple-touch-icon', sizes: '57x57', href: '/assets/icon/v1/apple-icon-57x57.png' },
-    { rel: 'apple-touch-icon', sizes: '60x60', href: '/assets/icon/v1/apple-icon-60x60.png' },
-    { rel: 'apple-touch-icon', sizes: '72x72', href: '/assets/icon/v1/apple-icon-72x72.png' },
-    { rel: 'apple-touch-icon', sizes: '76x76', href: '/assets/icon/v1/apple-icon-76x76.png' },
-    { rel: 'apple-touch-icon', sizes: '114x114', href: '/assets/icon/v1/apple-icon-114x114.png' },
-    { rel: 'apple-touch-icon', sizes: '120x120', href: '/assets/icon/v1/apple-icon-120x120.png' },
-    { rel: 'apple-touch-icon', sizes: '144x144', href: '/assets/icon/v1/apple-icon-144x144.png' },
-    { rel: 'apple-touch-icon', sizes: '152x152', href: '/assets/icon/v1/apple-icon-152x152.png' },
-    { rel: 'apple-touch-icon', sizes: '180x180', href: '/assets/icon/v1/apple-icon-180x180.png' },
+    { rel: 'apple-touch-icon', sizes: '57x57', href: '/assets/icon/v1.4/apple-icon-57x57.png' },
+    { rel: 'apple-touch-icon', sizes: '60x60', href: '/assets/icon/v1.4/apple-icon-60x60.png' },
+    { rel: 'apple-touch-icon', sizes: '72x72', href: '/assets/icon/v1.4/apple-icon-72x72.png' },
+    { rel: 'apple-touch-icon', sizes: '76x76', href: '/assets/icon/v1.4/apple-icon-76x76.png' },
+    { rel: 'apple-touch-icon', sizes: '114x114', href: '/assets/icon/v1.4/apple-icon-114x114.png' },
+    { rel: 'apple-touch-icon', sizes: '120x120', href: '/assets/icon/v1.4/apple-icon-120x120.png' },
+    { rel: 'apple-touch-icon', sizes: '144x144', href: '/assets/icon/v1.4/apple-icon-144x144.png' },
+    { rel: 'apple-touch-icon', sizes: '152x152', href: '/assets/icon/v1.4/apple-icon-152x152.png' },
+    { rel: 'apple-touch-icon', sizes: '180x180', href: '/assets/icon/v1.4/apple-icon-180x180.png' },
 
     /** <link> tags for a Web App Manifest **/
-    { rel: 'manifest', href: '/assets/manifest.json?v=1' }
+    { rel: 'manifest', href: '/assets/manifest.json?v=1.4' }
   ],
   meta: [
     { charset: 'utf-8' },
