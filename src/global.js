76f02b3777f1a59877b64eddaefcd1dc59c2f654
duxiaodong <duxiaodong@darlin.me>
2024-04-28 13:06:35 +0800

    fix: media

--- src/global.js
+++ src/global.js
@@ -1,7 +1,7 @@
 (function(g) {
   // Set interface in ./custom-typings.d.ts
   g.GLOBAL_VALUE = {
-    PIC_STATIC_URL_HOST: '//media.tristana.cc/'
+    PIC_STATIC_URL_HOST: '//proxy.darlin.me/media/'
     // PIC_STATIC_URL_HOST: '//darlin-me.oss-cn-shanghai.aliyuncs.com/'
     // PIC_STATIC_URL_HOST: '//media.darlin.me/'
   }
